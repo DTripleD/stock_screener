4a6dd19f4bd8c4c33fa7fbd1218494129f00df51
Danil Deineka <tripledpadik@gmail.com>
2023-04-25 23:45:00 +0300

    Bug fix

--- src/index.js
+++ src/index.js
@@ -1,43 +1,33 @@
 import debounce from 'lodash.debounce';
-import {showStocks} from './js/fetchData'
+import { showStocks } from './js/fetchData';
 
 import './js/holidaysGenerator';
 
-const input = document.querySelector("#stock-list");
-const btn = document.querySelector(".button");
-const list = document.querySelector(".stock-list");
-const form = document.querySelector(".form");
+const input = document.querySelector('#stock-list');
+const btn = document.querySelector('.button');
+const list = document.querySelector('.stock-list');
+const form = document.querySelector('.form');
 
+input.addEventListener('input', debounce(responseOfInput, 1000));
 
+function responseOfInput(event) {
+  event.preventDefault();
+  const stockNameInput = event.target.value;
 
-
-input.addEventListener("input", debounce(responseOfInput, 1000));
-
-
-
-function responseOfInput(event){
-    event.preventDefault();
-    const stockNameInput = event.target.value;
-
-    return showStocks(stockNameInput.toUpperCase()).then((data) => {
-        if (data.queryCount === 0) {
-            return 
-        }
-        return markup(data)});
+  return showStocks(stockNameInput.toUpperCase()).then(data => {
+    if (data.queryCount === 0) {
+      return;
+    }
+    return markup(data);
+  });
 }
 
-
-function markup(data){
-    console.log(data);
-    return list.innerHTML = listOfStocks(data);
+function markup(data) {
+  console.log(data);
+  return (list.innerHTML = listOfStocks(data));
 }
 
-function listOfStocks(data){
-    const {ticker} = data;
-    return `<li class="searced__stock list"><p>${ticker}</p></li>`
+function listOfStocks(data) {
+  const { ticker } = data;
+  return `<li class="searced__stock list"><p>${ticker}</p></li>`;
 }
-
-
-
-
-
--- src/js/holidaysGenerator.js
+++ src/js/holidaysGenerator.js
@@ -1,17 +1,21 @@
-import {marketHolidays} from '../js/fetchData'
+import { marketHolidays } from '../js/fetchData';
 
 // const calendar = document.querySelector(".calendar-list");
-const holydays = document.querySelector(".calendar-table");
+const holydays = document.querySelector('.calendar-table');
 
-marketHolidays().then((data) => markupForCalendar(data)).catch(console.log)
+marketHolidays()
+  .then(data => markupForCalendar(data))
+  .catch(console.log);
 
 function markupForCalendar(data) {
-    holydays.innerHTML = data.map(({date, exchange, name, status}) => {
-        return `<tr>
+  holydays.innerHTML = data
+    .map(({ date, exchange, name, status }) => {
+      return `<tr>
                     <td>${date}</td>
                     <td>${exchange}</td>
                     <td>${name}</td>
                     <td>${status}</td>
-                </tr>`
-    }).join("");
+                </tr>`;
+    })
+    .join('');
 }
